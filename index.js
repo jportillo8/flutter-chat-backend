db326e64416fe4fc8011f5069f03b9adb3fbce95
jportillo8 <buttokk@gmail.com>
2021-12-24 14:53:06 -0500

    Fin de la seccion 8

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
+// DB Config
+const { dbConnection } = require('./database/config');
+dbConnection();
+
+// App de Express
+const app = express();
+
+// Lectura y Parseo del body
+app.use( express.json());
+
+// Sokect node server
+const server = require('http').createServer(app);
+module.exports.io = require('socket.io')(server);
+require('./sockets/sokets');
+
+
+
+// Path publico
+const publicPath = path.resolve( __dirname, 'public');
+app.use(express.static( publicPath ));
+
+// Mis Rutas
+app.use( '/api/login', require('./routes/auth') );
+
+server.listen( process.env.PORT, (err) => {
+
+    if( err ) throw new Error(err);
+
+    console.log('Servidor corriendo en el puerto', process.env.PORT);
+
+})
---
+++ routes/auth.js
@@ -0,0 +1,36 @@
+/*
+   path: /api/login
+*/
+
+const { Router } = require('express');
+const { check } = require('express-validator');
+
+const { crearUsuario, login, renewToken } = require('../controllers/auth');
+const { validarCampos } = require('../middlewares/validar-campos');
+const { validarJWT } = require('../middlewares/validar-jwt');
+
+const router = Router();
+
+
+router.post('/new', [
+    check('nombre','El nombre es obligatorio').not().isEmpty(),
+    check('email', 'El correo es obligatorio').not().isEmpty(),
+    check('email', 'El correo debe ser Valido Ejemplo test@dominio.com').isEmail(),
+    check('password', 'El password  es obligatorio').not().isEmpty(),
+    check('password', 'Tu password es muy debil').isStrongPassword(),
+    validarCampos
+] , crearUsuario);
+
+
+
+router.post('/', [
+    check('email', 'El correo es obligatorio').not().isEmpty(),
+    check('email', 'El correo debe ser Valido Ejemplo test@dominio.com').isEmail(),
+    check('password', 'El password  es obligatorio').not().isEmpty(),
+    validarCampos
+], login);
+
+// Renovar JWT validarJWT
+router.get('/renew', validarJWT , renewToken );
+
+module.exports = router;
